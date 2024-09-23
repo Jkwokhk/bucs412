ce89d7de9e39023dc7694428e9dac66db2193874
Jkwokhk <jkwok.khk@gmail.com>
2024-09-23 15:13:46 -0400

    progress
    progress

--- hw3/urls.py
+++ hw3/urls.py
@@ -6,9 +6,9 @@ from . import views
 
 # All urls that are part of this app
 urlpatterns = [
-    path(r'', views.home, name="home"),
-    path(r'about', views.about, name="about"),
+    path(r'', views.quote, name="quote"),
     path(r'quote', views.quote, name="quote"),
+    path(r'about', views.about, name="about"),
     path(r'show_all', views.show_all, name="show_all"),
     
 ]
--- hw3/views.py
+++ hw3/views.py
@@ -4,35 +4,33 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 import random, time
 # Create your views here.
-def home(request):
-    '''
-    Function to handle the URL request for /hw (main page)
-    Delegate rendering to the template hw/ home.html
-    '''
-    # Use this template to render the response
-    template_name = 'hw3/home.html'
-    # create a dictionary of context variables for the template
-    context = {
-        "current_time": time.ctime(),
-        'letter1' : chr(random.randint(65, 90)), # letter from A to Z
-        'letter2' : chr(random.randint(65, 90)), # letter from A to Z
-        'number' : random.randint(1,10),
-    }
-    # delegate rendering work to the template
-    return render(request, template_name, context)
+# All the quotes here as a global variable
+quotes = [
+    "One good thing about music, when it hist you you feel no pain",
+    "The good times of today, are the sad thoughts of tomorrow",
+    "Money can't buy life",
+    "Every man gotta right to decide his own destiny",
+    "When one door is closed, don't you know, another one is open"
+]
+# All the images here as a global variable
+images = [
+    "https://i.ytimg.com/vi/CoabDBD1N2Q/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-BIAC4AOKAgwIABABGBEgcigkMA8=&rs=AOn4CLAdSw2p3OtYbALu5h3r4Kw3bMrX1g",
+    "https://hips.hearstapps.com/hmg-prod/images/gettyimages-78683778.jpg?crop=0.988xw:0.988xh;0.0119xw,0.0119xh&resize=640:*",
+    "https://www.rollingstone.com/wp-content/uploads/2018/06/rs-193885-541070899.jpg?w=1581&h=1054&crop=1",
+    "https://media.gq-magazine.co.uk/photos/5e3af120318f780008ca687b/master/pass/20191126-bob-marley-11.jpg",
+    "https://www.dancehallmag.com/assets/2024/01/Bob-Marley.jpg"
+]
+
 def about(request):
     '''
     Function to handle the URL request for /hw (main page)
-    Delegate rendering to the template hw/ home.html
+    Delegate rendering to the template hw3/ about.html
     '''
     # Use this template to render the response
     template_name = 'hw3/about.html'
     # create a dictionary of context variables for the template
     context = {
-        "current_time": time.ctime(),
-        'letter1' : chr(random.randint(65, 90)), # letter from A to Z
-        'letter2' : chr(random.randint(65, 90)), # letter from A to Z
-        'number' : random.randint(1,10),
+        
     }
     # delegate rendering work to the template
     return render(request, template_name, context)
@@ -43,12 +41,13 @@ def quote(request):
     '''
     # Use this template to render the response
     template_name = 'hw3/quote.html'
+    # Selecting a random quote and image 
+    quote = random.choice(quotes)
+    image = random.choice(images)
     # create a dictionary of context variables for the template
     context = {
-        "current_time": time.ctime(),
-        'letter1' : chr(random.randint(65, 90)), # letter from A to Z
-        'letter2' : chr(random.randint(65, 90)), # letter from A to Z
-        'number' : random.randint(1,10),
+        'quote': quote,
+        'image': image
     }
     # delegate rendering work to the template
     return render(request, template_name, context)
