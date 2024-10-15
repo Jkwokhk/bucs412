71d42f2d705c084a7310ee8ee0f5a49640b3f537
Jkwokhk <jkwok.khk@gmail.com>
2024-10-15 00:34:52 -0400

    update

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -3,8 +3,11 @@
 from django.urls import path
 from django.conf import settings
 from . import views
+from .views import *
 
 # All urls that are part of this app
 urlpatterns = [
+    
     path(r'', views.ShowAllProfilesView.as_view(), name="show_all_profiles"),
+    path(r'profile/<int:pk>', views.ShowProfilePageView.as_view(), name='show_profile'),
 ]
