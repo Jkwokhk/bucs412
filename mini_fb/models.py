58c624b9d0d642a55f86672ebf0bf23e455de85f
Jkwokhk <jkwok.khk@gmail.com>
2024-10-28 02:09:20 -0400

    task1

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -28,6 +28,14 @@ class Profile(models.Model):
     def get_absolute_url(self):
         # return url of this profile
         return reverse('show_profile', kwargs={'pk': self.pk})
+    
+    def get_friends(self):
+        # return list of friend's profiles
+        profile1_friends = Friend.objects.filter(profile1 =self)
+        profile2_friends = Friend.objects.filter(profile2=self)
+
+        friends = [friend.profile2 for friend in profile1_friends] + [friend.profile1 for friend in profile2_friends]
+        return friends
 
 class StatusMessage(models.Model):
     '''Encapsulate the idea of a status message in profile'''
@@ -54,3 +62,14 @@ class Image(models.Model):
     def __str__(self):
         '''return str representation of this image object'''
         return f'{self.status_message} at {self.timestamp}'
+    
+class Friend(models.Model):
+    '''encapsulates the idea of a friend(2 connecting nodes)'''
+    # data attributes of friend
+    profile1 = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile1')
+    profile2 = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile2')
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        '''str representation of Friend'''
+        return f"{self.profile1} and {self.profile2} at {self.timestamp}"
